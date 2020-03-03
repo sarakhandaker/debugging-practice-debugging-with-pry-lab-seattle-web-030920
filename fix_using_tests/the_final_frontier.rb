1933ad775f9b31964f1803c13fe4e850ee3cf5a4
Sara Khandaker <kttsara@hotmail.com>
2020-03-03 22:33:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,12 +12,11 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.each! {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
   date = generate_star_date
   state_log(date)
   greet_crew(crew)
-  binding.pry
 end
